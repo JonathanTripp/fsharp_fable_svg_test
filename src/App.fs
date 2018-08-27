af5280c8b0809f59a98aebef074473a6c169a5a4
Jonathan Tripp <jonathan@trippsoft.co.uk>
2018-08-27 18:55:07 +0100

    Try circles

--- src/App.fs
+++ src/App.fs
@@ -13,7 +13,12 @@ let renderLines () =
             (s=?"line") [ "x1" => 300; "y1" => 100; "x2" => 300; "y2" => 500; "style" => "stroke:black;stroke-width:1" ] [];
             (s=?"line") [ "x1" => 100; "y1" => 100; "x2" => 500; "y2" => 500; "style" => "stroke:blue;stroke-width:2" ] [];
             (s=?"line") [ "x1" => 100; "y1" => 500; "x2" => 500; "y2" => 100; "style" => "stroke:red;stroke-width:5" ] [];
-            (s=?"line") [ "x1" => 100; "y1" => 300; "x2" => 500; "y2" => 300; "style" => "stroke:green;stroke-width:10" ] []
+            (s=?"line") [ "x1" => 100; "y1" => 300; "x2" => 500; "y2" => 300; "style" => "stroke:green;stroke-width:10" ] [];
+
+            (s=?"circle") [ "cx" => 300; "cy" => 100; "r" => 25; "stroke" => "black"; "stroke-width" => 2; "fill" => "blue" ] [];
+            (s=?"circle") [ "cx" => 100; "cy" => 100; "r" => 25; "stroke" => "blue"; "stroke-width" => 5; "fill" => "red" ] [];
+            (s=?"circle") [ "cx" => 100; "cy" => 500; "r" => 25; "stroke" => "red"; "stroke-width" => 10; "fill" => "green" ] [];
+            (s=?"circle") [ "cx" => 100; "cy" => 300; "r" => 25; "stroke" => "green"; "stroke-width" => 20; "fill" => "black" ] [];
         ]
 
 let error msg =
